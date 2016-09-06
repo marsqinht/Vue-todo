bfe5ce93e10b19f21b4af99f045096169ce36398
Qin Haitao <qinhaitaocn@163.com>
2016-09-06 17:55:20 +0800

    todo-lsit

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,33 +1,44 @@
 <template>
   <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
+    <h1>{{aa}}</h1>
+    <input type="text" v-model='newItem' @keyup.enter="addItem">
+    <ul>
+      <li v-for='item in items' :class="{finished:item.isF}" @click="go(item)">
+        {{item.label}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
 export default {
-  components: {
-    Hello
+  data (){
+    return {
+      aa:'This is a todo-list',
+      items:[
+        {
+          label:'coding',
+          isF:true
+        },
+        {
+          label:'working',
+          isF:false
+        }
+      ],
+      newItem:''
+    }
+  },
+  methods:{
+      go(a){
+        a.isF=!a.isF
+      },
+      addItem(){
+        this.items.push({
+            label:this.newItem,
+            isF:false
+          })
+        this.newItem = '';
+      }
   }
 }
 </script>
@@ -36,7 +47,9 @@ export default {
 html {
   height: 100%;
 }
-
+.finished {
+  text-decoration: underline;
+}
 body {
   display: flex;
   align-items: center;
